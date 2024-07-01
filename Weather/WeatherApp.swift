b82819f4791b411cb3587b01b73329b5f0771917
Hannakaroline <karolinepalacios@gmail.com>
2024-07-01 10:54:37 -0400

    initial project

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Hanna on 01/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
